35bededcb81b4b0a59eb1f07504896e6728ae901
Joe Fox <joefox@Joes-MBP.lan>
2020-11-21 20:42:20 +0000

    Add basic bruteforce prime calculator

--- python_prime_calculator/calculator.py
+++ python_prime_calculator/calculator.py
@@ -13,8 +13,8 @@ class Calculator :
         self.threads = threads
 
     def validateArgs(self, min: int, max: int, threads: int) :
-        if min < 1 :
-            raise ValueError('min is not integer greater than 0')
+        if min < 2 :
+            raise ValueError('min is not integer greater than 1')
 
         if max <= min :
             raise ValueError('max is not integer greater than min')
@@ -23,4 +23,23 @@ class Calculator :
             raise ValueError('threads is not a positive integer')
 
     def run(self) :
-        print('run called')
+        primes    = 0
+        notPrimes = 0
+        for i in range(self.min, self.max) :
+            if self.testIsPrime(i) :
+                print(str(i) + ' is prime')
+                primes += 1
+            else :
+                print(str(i) + ' is not prime')
+                notPrimes += 1
+
+        print('')
+        print('Found ' + str(primes) + ' primes')
+        print('Found ' + str(notPrimes) + ' not primes')
+
+    def testIsPrime(self, test: int):
+        for i in range(2, test - 1) :
+            if (float(test)/i) == int(test/i) :
+                return False
+
+        return True
